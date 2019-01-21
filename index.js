b12ae57508dc613f522b07432673c189a3d86d5d
Sarah <sarah@3dgraphics.com>
2019-01-20 22:59:12 -0600

    Formatting again.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var fs = require("fs");
 var date = require('date-and-time');
 let now = new Date();
-console.log(date.format(now, 'YYYY/MM/DD HH:mm:ss'));    // => '2015/01/02 23:14:05'
-date.format(now, 'ddd MMM DD YYYY');        // => 'Fri Jan 02 2015'
+fs.readFile("./Formatted.csv", "utf8", function(err, data) {
+    if(err) throw err;
+    var formatted = data.toString().split("\n");
+    for(var i = 0; i < formatted.length; i++) {
+        formatted[i] = formatted[i].split(",");
+    }
+    console.log(formatted);
+});
