931e16ad1356bc4cfe73259bdc59f491bb27d9ac
Sarah <sarah@3dgraphics.com>
2019-02-17 23:50:08 -0600

    We have finished.

--- NeuralNet.py
+++ NeuralNet.py
@@ -2,18 +2,15 @@ from __future__ import absolute_import, division, print_function
 
 import tflearn
 import random
-# Regression data- 2 training instances
-#10 input features per instance.
+import numpy
 with open("./data.txt", "r") as f:
     data = eval(f.read())
 XX = []
 # So our data gives us the state, the date, the severity, and all the weights. We'll need to travel forward and get the weights in the future - if possible.
-# This is the part I'm kind of dreading...
 # One of our xs will be the array we get here. Our y will be data[i + 1][0]
 # X.push([])
 # data[i] = [state, date, weights]
 # We need to group them by state... and make a different NN. For each state.
-# Ohhhh dear.
 ourStuff = []
 while (len(data)):
     # Here we're splicing our stuff out
@@ -28,118 +25,33 @@ while (len(data)):
         # Do nothing
         print("Done with parsing")
     data.pop(0)
-#for i in XX:
-#print(i, "\n")
 #2 output features per instance
-#print(X)
-
-# Just 1d - for now.
-# Simple linear regression.
-for i in range(10, 11):
+for i in range(3, 5):
     # Name of state: XX[i][0]; data: XX[i][1]
     # So. With the data, our Xs will be our giant, long inputs. We'll push those into our X.
     X = []
     Y = []
     testinstance = []
-    print(XX[i][0])
     for ii in range(len(XX[i][1]) - 1):
         # Here we get our data
         X.append(XX[i][1][ii][1])
         Y.append([XX[i][1][ii + 1][0]])
-        if XX[i][1][ii][1][0] != 1:
-            testinstance.append([XX[i][1][ii][1], XX[i][1][ii + 1][0]])
-    input_layer = tflearn.input_data(shape=[None, len(X[0])])
-    dense1 = tflearn.fully_connected(
-        input_layer,
-        len(Y[0]),
-        activation='tanh',
-        regularizer='L2',
-        weight_decay=0.001)
-    dropout1 = tflearn.dropout(dense1, 0.5)
-    dense2 = tflearn.fully_connected(
-        dropout1,
-        len(Y[0]),
-        activation='tanh',
-        regularizer='L2',
-        weight_decay=0.001)
-    dropout2 = tflearn.dropout(dense2, 0.5)
-    softmax = tflearn.fully_connected(
-        dropout2, len(Y[0]), activation='softmax')
-
-    # Regression using SGD with learning rate decay and Top-3 accuracy
-    sgd = tflearn.SGD(learning_rate=0.1, lr_decay=0.96, decay_step=1000)
-    top_k = tflearn.metrics.Top_k(3)
-    net = tflearn.regression(
-        softmax, optimizer=sgd, metric=top_k, loss='categorical_crossentropy')
-
-    # Training
-    model = tflearn.DNN(net, tensorboard_verbose=0)
-    model.fit(X, Y, n_epoch=40, show_metric=True, run_id="dense_model")
-    # print("\nInput: ", testinstance)
-    # print("\nOutput: ", model.predict(testinstance))
-    for ii in range(len(testinstance)):
-        print("\nInput: ", [testinstance[ii][0]])
-        print("\nOutput: ", model.predict([testinstance[ii][0]]))
-        print("\nActual Output: ", testinstance[ii][1])
-    # input_ = tflearn.input_data(shape=[None, len(X[0])])
-    # r1 = tflearn.fully_connected(input_, 10)
-    # #2-d fully connected layer for output
-    # r1 = tflearn.fully_connected(r1, 1)
-    # r1 = tflearn.regression(
-    #     r1,
-    #     optimizer='sgd',
-    #     loss='mean_square',
-    #     metric='R2',
-    #     learning_rate=0.001)
-    # m = tflearn.DNN(r1)
-    # m.fit(X, Y, n_epoch=400, show_metric=True, snapshot_epoch=True)
-    # #Predict for 1 instance
-    # # This one instance will be the last of the range on line 42
-    # testinstance = [XX[i][1][len(XX[i][1]) - 1][1]]
-    # print("\nInput features:  ", testinstance)
-    # print("\n Predicted output: ")
-    # print(m.predict(testinstance))
-
-# dataIn = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]]
-# dataOut = [[4], [7]]
-# input_ = tflearn.input_data(shape=[None, len(dataIn[0])])
-# r1 = tflearn.fully_connected(input_, 10)
-# #2-d fully connected layer for output
-# r1 = tflearn.fully_connected(r1, 1)
-# r1 = tflearn.regression(
-#     r1, optimizer='sgd', loss='mean_square', metric='R2', learning_rate=0.001)
-# m = tflearn.DNN(r1)
-# m.fit(dataIn, dataOut, n_epoch=400, show_metric=True, snapshot_epoch=True)
-# #Predict for 1 instance
-# # This one instance will be the last of the range on line 42
-# testinstance = [[2, 3, 4, 5, 6, 7, 8, 9, 10, 11]]
-# print("\nInput features:  ", testinstance)
-# print("\n Predicted output: ")
-# print(m.predict(testinstance))
-""" for i in range(1):
-    # print(XX[i])
-    # Our x will be XX[1][1]
-    # Y = []
-    # X = []
-    # for ii in range(len(XX[i][1]) - 1):
-    #     X.append(XX[i][1][ii][1])
-    #     Y.append([XX[i][1][ii + 1][0]])
-    #     # print(XX[i][0], XX[i][1][ii][1], XX[i][1][ii + 1][0]) # Returns state, [severities], expected severity
-    # print(X, "\n\n")
-    input_ = tflearn.input_data(shape=[None, len(X[0])])
-    r1 = tflearn.fully_connected(input_,10)
+        testinstance.append([XX[i][1][ii][1], XX[i][1][ii + 1][0]])
+    input_ = tflearn.input_data(shape=[None,len(X[0])])
+    #10-d fully connected layer
+    r1 = tflearn.fully_connected(input_,len(X[0]))
+    # r1 = tflearn.fully_connected(r1, len(X[0]))
+    # r1 = tflearn.fully_connected(r1, len(X[0]))
+    r1 = tflearn.fully_connected(r1, len(X[0]), activation="relu")
     #2-d fully connected layer for output
     r1 = tflearn.fully_connected(r1, len(Y[0]))
     r1 = tflearn.regression(r1, optimizer='sgd', loss='mean_square',
-                                        metric='R2', learning_rate=0.00001)
-    m = tflearn.DNN(r1)
-    m.fit(X,Y, n_epoch=200, show_metric=True, snapshot_epoch=True)
+                                            metric='R2', learning_rate=0.01)
+    m = tflearn.DNN(r1, tensorboard_dir='tflearn_logs')
+    # print(len(X), len(Y))
+    print(X, Y)
+    m.fit(X,Y, n_epoch=50, show_metric=True, snapshot_epoch=False, run_id=XX[i][0])
     #Predict for 1 instance
-    # This one instance will be the last of the range on line 42
-    pos = int(round(random.uniform(0, len(XX[i][1]) - 2), 0))
-    testinstance=[XX[i][1][pos][1]]
-    print("\nInput features:  ",testinstance)
-    print("\n Predicted output: ")
-    print(m.predict(testinstance))
-    print("Hopefully our output: ", XX[i][1][pos + 1][0])
- """
+    # weights = m.get_weights(r1.W)
+    X = []
+    Y = []
--- index1.js
+++ index1.js
@@ -1,55 +1,95 @@
-var fs = require("fs");
-fs.readFile("./Formatted.csv", function (err, data) {
-    if (err) throw err;
-    var content = data.toString().split("\n");
-    // Combine by states.
-    var state = [],
-        group = "";
-    for (var i = 0; i < content.length; i++) {
-        content[i] = content[i].split(",");
-    }
-    content = content.sort(function (a, b) {
-        return a[0].localeCompare(b[0]);
-    })
-    while (content.length) {
-        if (content[1]) {
-            if (content[0][0] == content[1][0]) {
-                group += content[0].slice(1, content[0].length) + "\n";
+// ****IMPORTANT FILE!!!! READS INFO.CSV AND PUSHES OUT DATA.TXT!!!****
+var fs = require("fs"),
+    csv = require("csvtojson");
+var listOfStates = [
+    'Virgin Islands',
+    'Puerto Rico',
+    'Utah',
+    'Texas',
+    'Colorado',
+    'Louisiana',
+    'Kentucky',
+    'Kansas',
+    'Iowa',
+    'Indiana',
+    'Illinois',
+    'Idaho',
+    'Hawaii',
+    'Georgia',
+    'Florida',
+    'California',
+    'Arkansas',
+    'Arizona',
+    'Alaska',
+    'Alabama',
+    'Delaware',
+    'Connecticut',
+    'Tennessee',
+    'South Dakota',
+    'South Carolina',
+    'District of Columbia',
+    'Pennsylvania',
+    'Oregon',
+    'Oklahoma',
+    'Ohio',
+    'North Dakota',
+    'North Carolina',
+    'New York',
+    'New Mexico',
+    'New Jersey',
+    'New Hampshire',
+    'Nevada',
+    'Nebraska',
+    'Montana',
+    'Missouri',
+    'Mississippi',
+    'Minnesota',
+    'Michigan',
+    'Massachusetts',
+    'Maryland',
+    'Maine',
+    'Wyoming',
+    'Wisconsin',
+    'West Virginia',
+    'Washington',
+    'Virginia',
+    'Vermont',
+    'Rhode Island',
+    'District of Columbia'
+]
+csv()
+    .fromFile("./info.csv")
+    .then((data) => {
+        var answer = [];
+        for (var i = 0; i < data.length; i++) {
+            // 1) We need to look at each state, and the date.
+            // field1 is our date.
+            // Then we push in all the values in there.
+            var weights = [],
+                values = Object.getOwnPropertyNames(data[i]);
+            for (var ii = 1; ii < values.length; ii++) {
+                // data[values[ii]] gives severity.
+                weights.push(Number(data[i][values[ii]]));
+            }
+            //console.log(weights + "\n");
+            // Now for each state in here we need to push [state, date, severity, [weights]].
+            for (var ii = 1; ii < values.length; ii++) {
+                answer.push([values[ii], data[i][values[0]], weights[ii - 1], weights]);
+            }
+        }
+        //console.log(JSON.stringify(answer, null, "  "));
+        // We did it! Now order this by date.
+        answer = answer.sort(function (a, b) {
+            if (a[0].localeCompare(b[0]) !== 0) {
+                return a[0].localeCompare(b[0]);
             }
             else {
-                // Finished with this
-                state.push([content[0][0], group]);
-                group = "";
+                return new Date(a[1]) - new Date(b[1]);
             }
-        }
-        else {
-            // Last bit...
-            state.push([content[0][0], group.slice(0, -1)]); // The last one will just be ""
-            group = "";
-        }
-        content.splice(0, 1);
-    }
-    for (var i = 0; i < state.length; i++) {
-        state[i][1] = state[i][1].split("\n");
-        for (var ii = 0; ii < state[i][1].length; ii++) {
-            state[i][1][ii] = state[i][1][ii].split(",");
-        }
-    }
-    for (var i = 0; i < state.length; i++) {
-        // state[i][1] is the data
-        // state[i][1][1] is our date.
-        state[i][1] = state[i][1].sort(function (a, b) {
-            return new Date(a[1]) - new Date(b[1]);
         });
-        state[i][1] = state[i][1].map(function (val) {
-            val[0] = Number(val[0].split("Level ").join(""));
-            return val;
+        // console.log(JSON.stringify(answer, null, "  "));
+        fs.writeFile("./data.txt", JSON.stringify(answer, null, "  "), function (err) {
+            if (err) return err;
+            console.log("Finished formatting; see data.txt for file");
         });
-    }
-    fs.writeFile("./NeuralDataParsed.txt", JSON.stringify(state, null, " "), function (err) {
-        if (err) throw err;
-        console.log("Wrote to file NeuralDataParsed.txt");
     });
-    // console.log(state[0][1] == state[1][1]); // the test as to whether all this work was worth it...
-    // Now we compare the dates, remove the dates, and push that all into a new function. Are you ready?
-});
